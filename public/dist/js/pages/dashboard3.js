52dbff66dfba878964e8dab082595fa592534e84
Ziza Firsta Mahadewi <160825917+zizafirsta@users.noreply.github.com>
2026-05-12 20:30:09 +0700

    ziza(2023-175)Interface Segregation Principle (ISP)

--- public/dist/js/pages/dashboard3.js
+++ public/dist/js/pages/dashboard3.js
@@ -1,61 +1,64 @@
 
+
 $(function () {
   'use strict'
 
+  const BASE_TICKS = { fontColor: '#495057', fontStyle: 'bold', beginAtZero: true };
+  const BASE_GRID  = { display: true, lineWidth: '4px', color: 'rgba(0, 0, 0, .2)', zeroLineColor: 'transparent' };
+  
 
-  const chartStyles = {
-    ticks: { fontColor: '#495057', fontStyle: 'bold' },
-    gridLines: {
-      display: true,
-      lineWidth: '4px',
-      color: 'rgba(0, 0, 0, .2)',
-      zeroLineColor: 'transparent'
-    }
+  const YAxisInterfaces = {
+    standard: () => ({
+      gridLines: BASE_GRID,
+      ticks: BASE_TICKS
+    }),
+    withCurrency: () => ({
+      gridLines: BASE_GRID,
+      ticks: $.extend({}, BASE_TICKS, {
+        callback: (value) => '$' + (value >= 1000 ? (value / 1000) + 'k' : value)
+      })
+    }),
+    withMax: (max) => ({
+      gridLines: BASE_GRID,
+      ticks: $.extend({}, BASE_TICKS, { suggestedMax: max })
+    })
   };
 
-  const interactionMode = { mode: 'index', intersect: true };
-
-  const chartUtils = {
-    formatToK: (value) => (value >= 1000 ? (value / 1000) + 'k' : value)
+  const DatasetBuilders = {
+    bar: (data, color) => ({
+      type: 'bar',
+      data: data,
+      backgroundColor: color,
+      borderColor: color
+    }),
+    line: (data, color) => ({
+      type: 'line',
+      data: data,
+      backgroundColor: 'transparent',
+      borderColor: color,
+      pointBorderColor: color,
+      pointBackgroundColor: color,
+      fill: false
+    })
   };
 
-
-  const DataGenerator = {
-    createDataset: function(config) {
-     
-      return {
-        label: config.label || '',
-        data: config.data || [],
-        backgroundColor: config.backgroundColor || 'transparent',
-        borderColor: config.borderColor || '#000',
-        pointBorderColor: config.borderColor || '#000',
-        pointBackgroundColor: config.borderColor || '#000',
-        fill: config.fill || false,
-        type: config.type || undefined // Bisa Bar atau Line
-      };
-    }
-  };
-
-  const createChart = function ($element, type, chartData, extraYAxisOptions = {}) {
-    if (!$element.length) return; 
+  const createChart = function ($element, type, labels, datasets, yAxisConfig) {
+    if (!$element.length) return;
 
     return new Chart($element, {
       type: type,
-      data: chartData,
+      data: { labels, datasets },
       options: {
         maintainAspectRatio: false,
-        tooltips: interactionMode,
-        hover: interactionMode,
         legend: { display: false },
+        tooltips: { mode: 'index', intersect: true },
+        hover: { mode: 'index', intersect: true },
         scales: {
-          yAxes: [{
-            gridLines: chartStyles.gridLines,
-            ticks: $.extend({ beginAtZero: true }, chartStyles.ticks, extraYAxisOptions)
-          }],
+          yAxes: [yAxisConfig],
           xAxes: [{
             display: true,
             gridLines: { display: false },
-            ticks: chartStyles.ticks
+            ticks: BASE_TICKS
           }]
         }
       }
@@ -63,44 +66,30 @@ $(function () {
   };
 
 
-  const salesData = {
-    labels: ['JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'],
-    datasets: [
-      DataGenerator.createDataset({
-        data: [1000, 2000, 3000, 2500, 2700, 2500, 3000],
-        backgroundColor: '#007bff',
-        borderColor: '#007bff'
-      }),
-      DataGenerator.createDataset({
-        data: [700, 1700, 2700, 2000, 1800, 1500, 2000],
-        backgroundColor: '#ced4da',
-        borderColor: '#ced4da'
-      })
-    ]
-  };
+  const salesDatasets = [
+    DatasetBuilders.bar([1000, 2000, 3000, 2500, 2700, 2500, 3000], '#007bff'),
+    DatasetBuilders.bar([700, 1700, 2700, 2000, 1800, 1500, 2000], '#ced4da')
+  ];
 
-  createChart($('#sales-chart'), 'bar', salesData, {
-    callback: (val) => '$' + chartUtils.formatToK(val)
-  });
+  createChart(
+    $('#sales-chart'), 
+    'bar', 
+    ['JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'], 
+    salesDatasets, 
+    YAxisInterfaces.withCurrency()
+  );
 
+  // Visitors Chart: Hanya menggunakan Interface Max & Builder Line
+  const visitorsDatasets = [
+    DatasetBuilders.line([100, 120, 170, 167, 180, 177, 160], '#007bff'),
+    DatasetBuilders.line([60, 80, 70, 67, 80, 77, 100], '#ced4da')
+  ];
 
-  const visitorsData = {
-    labels: ['18th', '20th', '22nd', '24th', '26th', '28th', '30th'],
-    datasets: [
-      DataGenerator.createDataset({
-        type: 'line',
-        data: [100, 120, 170, 167, 180, 177, 160],
-        borderColor: '#007bff'
-      }),
-      DataGenerator.createDataset({
-        type: 'line',
-        data: [60, 80, 70, 67, 80, 77, 100],
-        borderColor: '#ced4da'
-      })
-    ]
-  };
-
-  createChart($('#visitors-chart'), null, visitorsData, {
-    suggestedMax: 200
-  });
+  createChart(
+    $('#visitors-chart'), 
+    null, 
+    ['18th', '20th', '22nd', '24th', '26th', '28th', '30th'], 
+    visitorsDatasets, 
+    YAxisInterfaces.withMax(200)
+  );
 });
