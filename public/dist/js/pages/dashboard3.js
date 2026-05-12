c7c47a9242ed66671d2ad37c74bde101e2ab2517
Ziza Firsta Mahadewi <160825917+zizafirsta@users.noreply.github.com>
2026-05-12 20:26:47 +0700

    ziza(2023-175)Single Responsibility Principle (SRP)

--- public/dist/js/pages/dashboard3.js
+++ public/dist/js/pages/dashboard3.js
@@ -1,138 +1,121 @@
+
+
+
 $(function () {
   'use strict'
 
-  var ticksStyle = {
-    fontColor: '#495057',
-    fontStyle: 'bold'
-  }
+  var chartStyles = {
+    ticks: {
+      fontColor: '#495057',
+      fontStyle: 'bold'
+    },
+    gridLines: {
+      display: true,
+      lineWidth: '4px',
+      color: 'rgba(0, 0, 0, .2)',
+      zeroLineColor: 'transparent'
+    }
+  };
 
-  var mode      = 'index'
-  var intersect = true
+  var interactionMode = {
+    mode: 'index',
+    intersect: true
+  };
 
+
+  var chartUtils = {
+    formatToK: function (value) {
+      if (value >= 1000) {
+        value /= 1000
+        value += 'k'
+      }
+      return '$' + value
+    }
+  };
+
+  // --- 3. INISIALISASI SALES CHART ---
   var $salesChart = $('#sales-chart')
-  var salesChart  = new Chart($salesChart, {
-    type   : 'bar',
-    data   : {
-      labels  : ['JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'],
+  var salesChart = new Chart($salesChart, {
+    type: 'bar',
+    data: {
+      labels: ['JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'],
       datasets: [
         {
           backgroundColor: '#007bff',
-          borderColor    : '#007bff',
-          data           : [1000, 2000, 3000, 2500, 2700, 2500, 3000]
+          borderColor: '#007bff',
+          data: [1000, 2000, 3000, 2500, 2700, 2500, 3000]
         },
         {
           backgroundColor: '#ced4da',
-          borderColor    : '#ced4da',
-          data           : [700, 1700, 2700, 2000, 1800, 1500, 2000]
+          borderColor: '#ced4da',
+          data: [700, 1700, 2700, 2000, 1800, 1500, 2000]
         }
       ]
     },
     options: {
       maintainAspectRatio: false,
-      tooltips           : {
-        mode     : mode,
-        intersect: intersect
-      },
-      hover              : {
-        mode     : mode,
-        intersect: intersect
-      },
-      legend             : {
-        display: false
-      },
-      scales             : {
+      tooltips: interactionMode,
+      hover: interactionMode,
+      legend: { display: false },
+      scales: {
         yAxes: [{
-          // display: false,
-          gridLines: {
-            display      : true,
-            lineWidth    : '4px',
-            color        : 'rgba(0, 0, 0, .2)',
-            zeroLineColor: 'transparent'
-          },
-          ticks    : $.extend({
+          gridLines: chartStyles.gridLines,
+          ticks: $.extend({
             beginAtZero: true,
-
-            // Include a dollar sign in the ticks
-            callback: function (value, index, values) {
-              if (value >= 1000) {
-                value /= 1000
-                value += 'k'
-              }
-              return '$' + value
-            }
-          }, ticksStyle)
+            callback: chartUtils.formatToK // Menggunakan utility formatter
+          }, chartStyles.ticks)
         }],
         xAxes: [{
-          display  : true,
-          gridLines: {
-            display: false
-          },
-          ticks    : ticksStyle
+          display: true,
+          gridLines: { display: false },
+          ticks: chartStyles.ticks
         }]
       }
     }
   })
 
+
   var $visitorsChart = $('#visitors-chart')
-  var visitorsChart  = new Chart($visitorsChart, {
-    data   : {
-      labels  : ['18th', '20th', '22nd', '24th', '26th', '28th', '30th'],
+  var visitorsChart = new Chart($visitorsChart, {
+    // Note: Type didefinisikan di dalam dataset (Mixed Chart)
+    data: {
+      labels: ['18th', '20th', '22nd', '24th', '26th', '28th', '30th'],
       datasets: [{
-        type                : 'line',
-        data                : [100, 120, 170, 167, 180, 177, 160],
-        backgroundColor     : 'transparent',
-        borderColor         : '#007bff',
-        pointBorderColor    : '#007bff',
+        type: 'line',
+        data: [100, 120, 170, 167, 180, 177, 160],
+        backgroundColor: 'transparent',
+        borderColor: '#007bff',
+        pointBorderColor: '#007bff',
         pointBackgroundColor: '#007bff',
-        fill                : false
-        // pointHoverBackgroundColor: '#007bff',
-        // pointHoverBorderColor    : '#007bff'
+        fill: false
       },
-        {
-          type                : 'line',
-          data                : [60, 80, 70, 67, 80, 77, 100],
-          backgroundColor     : 'tansparent',
-          borderColor         : '#ced4da',
-          pointBorderColor    : '#ced4da',
-          pointBackgroundColor: '#ced4da',
-          fill                : false
-          // pointHoverBackgroundColor: '#ced4da',
-          // pointHoverBorderColor    : '#ced4da'
-        }]
+      {
+        type: 'line',
+        data: [60, 80, 70, 67, 80, 77, 100],
+        backgroundColor: 'transparent',
+        borderColor: '#ced4da',
+        pointBorderColor: '#ced4da',
+        pointBackgroundColor: '#ced4da',
+        fill: false
+      }]
     },
     options: {
       maintainAspectRatio: false,
-      tooltips           : {
-        mode     : mode,
-        intersect: intersect
-      },
-      hover              : {
-        mode     : mode,
-        intersect: intersect
-      },
-      legend             : {
-        display: false
-      },
-      scales             : {
+      tooltips: interactionMode,
+      hover: interactionMode,
+      legend: { display: false },
+      scales: {
         yAxes: [{
-          // display: false,
-          gridLines: {
-            display      : true,
-            lineWidth    : '4px',
-            color        : 'rgba(0, 0, 0, .2)',
-            zeroLineColor: 'transparent'
-          },
-          ticks    : $.extend({
-            beginAtZero : true,
+          gridLines: chartStyles.gridLines,
+          ticks: $.extend({
+            beginAtZero: true,
             suggestedMax: 200
-          }, ticksStyle)
+          }, chartStyles.ticks)
         }],
         xAxes: [{
-          display  : true,
-          gridLines: {
-            display: false
-          },
-          ticks    : ticksStyle
+          display: true,
+          gridLines: { display: false },
+          ticks: chartStyles.ticks
         }]
       }
     }
