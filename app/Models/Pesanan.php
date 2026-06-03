b6b98e9c63d951c6425d933d9e7d7d0778d77b1d
zizafirsta <firstadewi14@gmail.com>
2026-06-03 13:17:16 +0700

    Add unit test for Pesanan model

--- app/Models/Pesanan.php
+++ app/Models/Pesanan.php
@@ -24,4 +24,42 @@ class Pesanan extends Model
     protected $casts = [
         'tanggal' => 'date',
     ];
+
+    <?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pesanan extends Model
+{
+    use HasFactory;
+
+    protected $table = 'pesanan';
+
+    protected $fillable = [
+        'code_pemesanan',
+        'nama',
+        'tanggal',
+        'produk',
+        'harga',
+        'status',
+        'bukti_pembayaran',
+    ];
+
+    protected $casts = [
+        'tanggal' => 'date',
+    ];
+
+    /**
+     * Tambahkan fungsi ini untuk logika bisnis yang akan di-test
+     * Mengecek apakah status pesanan sudah bernilai 'selesai'
+     */
+    public function isSelesai(): bool
+    {
+        return $this->status === 'selesai';
+    }
+}
+
 }
